32849487499fd97d99f545cf1b58376f7993409a
Onaleye <onaleyeolayinka@gmail.com>
2023-09-30 17:35:05 +0100

    Initial commit

---
+++ src/test/java/BaseClass/BaseClass.java
@@ -0,0 +1,13 @@
+package BaseClass;
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+
+    WebDriver driver;
+       // driver = new ChromeDriver();
+      //  driver.manage().deleteAllCookies();
+       // driver.manage().window().maximize();
+    String PageTitle = "Your Store";
+    //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      //  driver.get("http://tutorialsninja.com/demo/");
+}
